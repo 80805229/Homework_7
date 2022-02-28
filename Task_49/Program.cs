054680b7bac5cc8d28cd05d48eff20b329379bf1
Anna <aanikonova15@gmail.com>
2022-02-28 22:27:06 +0300

    Показать двумерный массив размером m×n, заполненный вещественными числами.

---
+++ Task_49/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать двумерный массив размером m×n, заполненный вещественными числами.
+
+
+Console.WriteLine("Введите размер массива m×n ");
+Console.Write("m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m,n];
+
+for (int i = 0; i<m;i++)
+{
+    for (int j=0; j<n; j++)
+    {
+        array[i,j]= new Random().Next(0, 10) + new Random().NextDouble();
+        Console.Write(array[i,j] +" ");
+    }
+    Console.WriteLine();
+}
